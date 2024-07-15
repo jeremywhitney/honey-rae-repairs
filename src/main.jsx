bd73dd50fcd0487a278fe3e638b15c2b329e05f4
Jeremy Whitney <bearwhitness@gmail.com>
2024-07-15 09:44:40 -0500

    created navbar and routes for tickets, customers, employees

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,13 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import { App } from "./App.jsx"
-import "./index.css"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
-const container = document.getElementById("root")
-const root = ReactDOM.createRoot(container)
-root.render(<App />)
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
