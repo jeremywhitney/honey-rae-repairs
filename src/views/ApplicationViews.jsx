5125450f8b594c032b45315eaebabffdc6672725
Jeremy Whitney <bearwhitness@gmail.com>
2024-07-16 10:02:23 -0500

    created employee profile

--- src/views/ApplicationViews.jsx
+++ src/views/ApplicationViews.jsx
@@ -7,6 +7,7 @@ import { EmployeeDetails } from "../components/employees/EmployeeDetails";
 import { CustomerList } from "../components/customers/CustomersList";
 import { CustomerDetails } from "../components/customers/CustomerDetails";
 import { useEffect, useState } from "react";
+import { EmployeeForm } from "../components/forms/EmployeeForm";
 
 export const ApplicationViews = () => {
   const [currentUser, setCurrentUser] = useState({});
@@ -39,6 +40,7 @@ export const ApplicationViews = () => {
           <Route index element={<CustomerList />} />
           <Route path=":customerId" element={<CustomerDetails />} />
         </Route>
+        <Route path="profile" element={<EmployeeForm currentUser={currentUser}/>} />
       </Route>
     </Routes>
   );
